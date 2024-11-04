2201222013086fe497064b3ae8203b8912b53127
masum <promotionbd321@gmail.com>
2024-11-04 19:01:16 +0600

    fix this all

--- lib/app/utils/app_icons.dart
+++ lib/app/utils/app_icons.dart
@@ -3,7 +3,36 @@ class AppIcons {
   static const String basePath = "assets/icons";
 
 
-  static const String email = "$basePath/email.svg";
+  static const String contacts = "$basePath/contacts.svg";
+  static const String delete = "$basePath/delete.svg";
+  static const String deposit = "$basePath/deposite.svg";
+  static const String drawer = "$basePath/drawer.svg";
+  static const String dropDown = "$basePath/dropdown.svg";
+  static const String eye = "$basePath/eye.svg";
+  static const String eyeOff = "$basePath/eye_off.svg";
+  static const String faqs = "$basePath/faqs.svg";
+  static const String favoriteSelected = "$basePath/favorite_selected.svg";
+  static const String favoriteUnselected = "$basePath/favorite_unselected.svg";
+  static const String googleSignIn = "$basePath/google_sign_in.svg";
+  static const String historySelected = "$basePath/history_selected.svg";
+  static const String historyUnselected = "$basePath/history_unselected.svg";
+  static const String homeSelected = "$basePath/home_selected.svg";
+  static const String homeUnselected = "$basePath/home_unselected.svg";
+  static const String inviteFriends = "$basePath/invite_friends.svg";
+  static const String key = "$basePath/key.svg";
+  static const String notificationSelected = "$basePath/notification_selected.svg";
+  static const String notificationUnselected = "$basePath/notification_unselected.svg";
+  static const String photoCamera = "$basePath/photo_camera.svg";
+  static const String privacy = "$basePath/privacy.svg";
+  static const String profileSelected = "$basePath/profile_selected.svg";
+  static const String profileUnselected = "$basePath/profile_unselected.svg";
+  static const String rightCheck = "$basePath/right_check.svg";
+  static const String search = "$basePath/search.svg";
+  static const String settings = "$basePath/settings.svg";
+  static const String terms = "$basePath/terms.svg";
+  static const String transaction = "$basePath/transaction_log.svg";
+  static const String withdraw = "$basePath/withdraw.svg";
+  static const String x = "$basePath/x.svg";
 
 
 
