9bbee742bed63a6a285ad2957be3ce54a6de591e
masum <promotionbd321@gmail.com>
2024-11-04 18:22:26 +0600

    color completed

--- lib/app/utils/app_colors.dart
+++ lib/app/utils/app_colors.dart
@@ -4,7 +4,11 @@ class AppColors {
 
   ///============================ Red Series ===============================
 
-  static const Color buttonColor = Color(0xff6200AF);
+  static const Color gray500 = Color(0xff333333);
+  static const Color blue500 = Color(0xff053697);
+  static const Color green500  = Color(0xff2FC191);
+  static const Color grey400  = Color(0xffD2D2D2);
+  static const Color white50  = Color(0xffFFFFFF);
 
 
 }
