77dae5516de3f691c5c3f360df362541ff4e9177
masum <promotionbd321@gmail.com>
2024-11-09 11:15:16 +0600

    otp completed

--- lib/app/screens/authentication_screen/otp_screen/otp_screen.dart
+++ lib/app/screens/authentication_screen/otp_screen/otp_screen.dart
@@ -1,25 +1,69 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_screenutil/flutter_screenutil.dart';
+// import 'package:gap/gap.dart';
+// import 'package:get/get.dart';
+// import 'package:protippz/app/core/app_routes.dart';
+// import 'package:protippz/app/global/controllers/auth_controller/auth_controller.dart';
+// import 'package:protippz/app/global/widgets/custom_appbar/custom_appbar.dart';
+// import 'package:protippz/app/global/widgets/custom_button/custom_button.dart';
+// import 'package:protippz/app/global/widgets/custom_text/custom_text.dart';
+// import 'package:protippz/app/utils/app_colors.dart';
+// import 'package:protippz/app/utils/app_strings.dart';
+//
+// class OtpScreen extends StatelessWidget {
+//   OtpScreen({super.key});
+//
+//   final AuthController authController = Get.find<AuthController>();
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+
+//       body: Padding(
+//           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 24),
+//           child: Column(
+//             children: [
+//
+//
+//
+//
+//             ],
+//           )
+//       ),
+//     );
+//   }
+// }
+
+
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gap/gap.dart';
 import 'package:get/get.dart';
-import 'package:go_router/go_router.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
 import 'package:protippz/app/core/app_routes.dart';
-import 'package:protippz/app/global/controllers/auth_controller/auth_controller.dart';
 import 'package:protippz/app/global/widgets/custom_appbar/custom_appbar.dart';
 import 'package:protippz/app/global/widgets/custom_button/custom_button.dart';
 import 'package:protippz/app/global/widgets/custom_text/custom_text.dart';
 import 'package:protippz/app/utils/app_colors.dart';
 import 'package:protippz/app/utils/app_strings.dart';
 
-class OtpScreen extends StatelessWidget {
-  OtpScreen({super.key});
+class OtpScreen extends StatefulWidget {
+  const OtpScreen({super.key});
 
-  final AuthController authController = Get.find<AuthController>();
+  @override
+  State<OtpScreen> createState() => _ForgetOtpState();
+}
+
+class _ForgetOtpState extends State<OtpScreen> {
+
+
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.bg500,
+            backgroundColor: AppColors.bg500,
 
       ///========================veryFy Code Appbar===================
       appBar: const CustomAppBar(
@@ -27,15 +71,15 @@ class OtpScreen extends StatelessWidget {
         appBarContent: AppStrings.verifyCode,
       ),
       body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 24),
-          child: Column(
-            children: [
-              const CustomText(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+        child: Column(
+          children: [
+            ///=============================Header Text=============================
+                          const CustomText(
                 text: AppStrings.checkYourEmail,
                 fontWeight: FontWeight.w500,
                 fontSize: 22,
                 color: AppColors.blue500,
-                bottom: 10,
               ),
 
               // const CustomText(
@@ -48,35 +92,76 @@ class OtpScreen extends StatelessWidget {
               // ),
 
 
-               Gap(12.h),
+               Gap(50.h),
 
 
-                    const Row(
-                      children: [
-                        Spacer(),
-                        CustomText(
-                          text: AppStrings.resendOtp,
-                          fontWeight: FontWeight.w400,
-                          fontSize: 12,
-                          color: AppColors.blue500,
-                        ),
-                      ],
-                    ),
 
-              //==========================VeryFy Code Button=================
-               Gap(30.h),
-              CustomButton(
-                onTap: () {
-                      Get.toNamed(AppRoute.resetPasswordScreen);
-                },
-                title: AppStrings.verifyCode,
+            ///=============================Pin Code Text Field=============================
+            PinCodeTextField(
+              textStyle: const TextStyle(color: AppColors.gray500),
+              keyboardType: TextInputType.phone,
+              autoDisposeControllers: false,
+              cursorColor: AppColors.gray500,
+              appContext: context,
+              // controller: authenticationController.pinCodeController,
+              // onCompleted: (value) {
+              //   if (isSignUp == "true") {
+              //     authenticationController.activationCode = value;
+              //   } else {
+              //     authenticationController.otp = value;
+              //   }
+              // },
+              validator: (value) {
+                if (value!.length == 4) {
+                  return null; // Input is valid
+                } else {
+                  return "Please enter a 4-digit OTP code"; // Input is invalid
+                }
+              },
+              autoFocus: true,
+              pinTheme: PinTheme(
+                disabledColor: Colors.transparent,
+                shape: PinCodeFieldShape.box,
+                borderRadius: BorderRadius.circular(12),
+                fieldHeight: 49.h,
+                fieldWidth: 47,
+                activeFillColor: AppColors.white50,
+                selectedFillColor: AppColors.white50,
+                inactiveFillColor: AppColors.white50,
+                borderWidth: 0.5,
+                activeBorderWidth: 0.8,
+                selectedColor: AppColors.white50,
+                inactiveColor: AppColors.white50,
+                activeColor: AppColors.white50,
               ),
+              length: 4,
+              enableActiveFill: true,
+            ),
 
+            SizedBox(height: 50.h),
 
+            const Row(
+              children: [
+                Spacer(),
+                CustomText(
+                  text: AppStrings.resendOtp,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 12,
+                  color: AppColors.blue500,
+                ),
+              ],
+            ),
 
-
-            ],
-          )
+            //==========================VeryFy Code Button=================
+            Gap(30.h),
+            CustomButton(
+              onTap: () {
+                Get.toNamed(AppRoute.resetPasswordScreen);
+              },
+              title: AppStrings.verifyCode,
+            ),
+          ],
+        )
       ),
     );
   }
