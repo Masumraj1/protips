35b03b3cbe3e9ce8aaf932e69c107fc7125e533d
masum <promotionbd321@gmail.com>
2024-12-12 15:09:35 +0600

    some issue complete

--- lib/app/screens/transaction_screen/transaction_screen.dart
+++ lib/app/screens/transaction_screen/transaction_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:protippz/app/controller/payment_controller.dart';
+import 'package:protippz/app/global/helper/date_converter/date_converter.dart';
 import 'package:protippz/app/global/widgets/custom_appbar/custom_appbar.dart';
 import 'package:protippz/app/global/widgets/custom_loader/custom_loader.dart';
 import 'package:protippz/app/global/widgets/custom_text/custom_text.dart';
@@ -12,62 +13,65 @@ import 'package:protippz/app/utils/app_constants.dart';
 import 'package:protippz/app/utils/app_strings.dart';
 
 class TransactionScreen extends StatelessWidget {
-   TransactionScreen({super.key});
+  TransactionScreen({super.key});
 
+  final PaymentController paymentController = Get.find<PaymentController>();
 
-  final PaymentController  paymentController = Get.find<PaymentController>();
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      backgroundColor: AppColors.bg500,
-      appBar: const CustomAppBar(
-        appBarContent: AppStrings.transactionLog,
-        iconData: Icons.arrow_back,
-      ),
-      body:   Obx(() {
-        switch (paymentController.rxRequestStatus.value) {
-          case Status.loading:
-            return const CustomLoader(); // Show loading indicator
+    return Scaffold(
+        backgroundColor: AppColors.bg500,
+        appBar: const CustomAppBar(
+          appBarContent: AppStrings.transactionLog,
+          iconData: Icons.arrow_back,
+        ),
+        body: Obx(() {
+          switch (paymentController.rxRequestStatus.value) {
+            case Status.loading:
+              return const CustomLoader(); // Show loading indicator
 
-          case Status.internetError:
-            return NoInternetScreen(onTap: () {
-              paymentController.transaction();
-            });
+            case Status.internetError:
+              return NoInternetScreen(onTap: () {
+                paymentController.transaction();
+              });
 
-          case Status.error:
-            return GeneralErrorScreen(
-              onTap: () {
-                paymentController.transaction();// Retry fetching data on error
-              },
-            );
-
-          case Status.completed:
-          // Show empty state if no notifications are found
-            if (paymentController.transactionList.isEmpty) {
-              return const Center(
-                child: CustomText(
-                  text: "No Data Found",
-                  fontWeight: FontWeight.w500,
-                  fontSize: 16,
-                  color: AppColors.gray500,
-                ),
+            case Status.error:
+              return GeneralErrorScreen(
+                onTap: () {
+                  paymentController
+                      .transaction(); // Retry fetching data on error
+                },
               );
-            }
-            return      ListView.builder(
-          itemCount: 4,
-          itemBuilder: (context, index) {
-            final item = paymentController.transactionList[index];
-            return  HistoryCard(
-              isImage: false,
-              title:item.transactionType??"",
-              date: 'd',
-              time:item.amount.toString(),
-            );
-          },
-        );
-        }
-      }));
 
-
+            case Status.completed:
+              // Show empty state if no notifications are found
+              if (paymentController.transactionList.isEmpty) {
+                return const Center(
+                  child: CustomText(
+                    text: "No Data Found",
+                    fontWeight: FontWeight.w500,
+                    fontSize: 16,
+                    color: AppColors.gray500,
+                  ),
+                );
+              }
+              return ListView.builder(
+                itemCount: 4,
+                itemBuilder: (context, index) {
+                  final item = paymentController.transactionList[index];
+                  return HistoryCard(
+                    isImage: false,
+                    title: item.transactionType == "Deposit"
+                        ? "Deposit Funds"
+                        : (item.transactionType == "Withdraw"
+                            ? "Withdraw Funds"
+                            : "Unknown Transaction"),
+                    date: DateConverter.formatDate(item.createdAt),
+                    time: "\$${item.amount.toString()}",
+                  );
+                },
+              );
+          }
+        }));
   }
 }
